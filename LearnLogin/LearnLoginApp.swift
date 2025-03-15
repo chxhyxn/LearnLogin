a384d3b2d63880f58eed151a772a8448995d9803
chxhyxn <chxhyxn@gmail.com>
2025-03-15 10:07:51 +0900

    Initial Commit

---
+++ LearnLogin/LearnLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnLoginApp.swift
+//  LearnLogin
+//
+//  Created by SeanCho on 3/15/25.
+//
+
+import SwiftUI
+
+@main
+struct LearnLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
